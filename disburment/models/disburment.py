77a9f087cfe46907ab11d612f00019c29c13948d
Victor Martin <vicmmt@gmail.com>
2022-04-14 17:22:47 +0200

    Update disburment.py

--- disburment/models/disburment.py
+++ disburment/models/disburment.py
@@ -3,6 +3,13 @@ import orm.fields
 import orm.make_response, orm.request
 import date.datetime
 
+
+class interval(models.model):
+	merchant_id = fields.many2one('merchants') 
+	quant_inf = fields.integer()
+	quant_sup = fields.integer()
+	commission = fields.float()
+		
 # models.model represent base model with id, creation_date, name
 class disburment(models.model):
 	merchant_id = fields.many2one('merchants') 
@@ -33,15 +40,18 @@ class disburment(models.model):
 		orders = self.env('orders').search(['|', ('completed_at', '>=', past_monday), ('completed_at', '<=', past_sunday)])
 		merchants = set([order.merchant_id for order in orders])
 		for merchant in merchants:
+			intervals = self.env('interval').search(['merchant_id ={}'.format(merchant.id)])
 			disburment_sum = 0.0
 			orders_to_disburment = orders.filtered(['merchant_id', '=', merchant])
 			for order in orders_to_disburment:
-				if order.amount < 50:
-					disburment_sum += order.amount * 0.01
-				elif order.amount > 50 and order.amount < 300:
-					disburment_sum += order.amount * 0.095
-				elif order.amount > 300:
-					disburment_sum += order.amount * 0.085
+				for interval in intevals:
+					if interval.quant_inf > 0.0 and interval.quant_sup > 0.0:
+						if order.amount > interval.quant_inf and order.amount < quant_sup:
+							disburment_sum += order.amount * interval.commission
+					elif interval.quant_inf > 0.0:
+						if order.amount > interval.quant_inf:
+							disburment_sum += order.amount * interval.commission
+							
 			self.env('disburment').create({
 					'merchant_id': merchant.id,
 					'week': monday.week,
